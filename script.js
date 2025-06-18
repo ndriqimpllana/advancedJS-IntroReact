1d81e868533ac3fd454a4906f50a66f87b5fb815
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-06-18 16:19:57 -0400

    changes in script.js

--- script.js
+++ script.js
@@ -1,81 +1,50 @@
-function greetEveryone(names) {
-    for (let i=0; i < names.length; i++) {
-        console.log("Hi," + names[i]);
+let car = {
+    name: 'Lightning McQueen',
+    model: 'Race Car',
+    year: 2006,
+    start: function() {
+        console.log(`Kachow!" ${this.year} ${this.name} is ready to roll!`);
+    },
+
+drive: function() {
+    console.log(`${this.name} is zooming at top speed! Vroom-vroom!`);
+},
+brake: function() {
+    console.log(`${this.name} creeches to a halt!`);
+},
+honk: function() {
+    console.log(`${this.name} honks: "beep beep in the clouds!!`);
+}
+};
+
+
+car.start();
+
+
+let student = {
+    name: "alex",
+    age: 21,
+    enrolled: true,
+    courses: ["javascript", "python", "html"],
+    greet: function() {
+        console.log(`Hello, I am ${this.name}`);
     }
+};
+
+student.city = "New York";
+student.greet();
+student.courses.push("css");
+console.log(student.courses);
+console.log(student);
+
+
+let person = {
+    name: "alice",
+    age: 40,
+    sex: true
+};
+
+for ( let key in person) {
+    console.log(`${key}: ${person[key]}`);
 }
 
-greetEveryone(["Ali", "Sara", "John"]);
-
-
-function filterPositives(numbers) {
-    let result = [];
-    for(let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > 0) {
-            result.push(numbers[i]);
-        }
-    }
-    return result
-}
-console.log(filterPositives([-1, 2, -3, 4, 5]));
-
-function sumNumbers(numbers) {
-    let total = 0;
-    for (let i = 0; i < numbers.length; i ++) {
-        total += numbers[i];
-    }
-    return total;
-}
-console.log(sumNumbers([10, 20, 30, 40]));
-
-
-function itsEven(num) {
-    return num % 2 === 0;
-}
-
-console.log(itsEven(6)); // true
-console.log(itsEven(7)); // false
-
-function isOdd(num) {
-    return num % 2 !== 0;
-}
-
-console.log(isOdd(6)); // false
-console.log(isOdd(7)); // true
-
-
-function greetUser() {
-    console.log("hello, student!");
-}
-
-greetUser(); // Output: hello, student!
-
-function addTwoNumbers(a, b) {
-    return a + b;
-}
-
-console.log(addTwoNumbers(4, 19));
-
-
-function greet(name) {
-    console.log("hello," + name);
-}
-
-greet("Ali"); 
-
-function countToFive() {
-    for (let i = 0; i <=5; i++) {
-        console.log(i);
-    }
-}
-
-countToFive();
-
-function printRange(n) {
-    for (let i = 0; i <= n; i++) {
-        console.log(i)
-    }
-}
-
-printRange(20); 
-
-function
