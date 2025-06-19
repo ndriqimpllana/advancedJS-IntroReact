b8804d0f71f3c5268c6438374e1385f214d69f8d
Ndricim Pllana <ndriqimpllana@gmail.com>
2025-06-18 20:06:16 -0400

    changes to the script.js

--- script.js
+++ script.js
@@ -1,50 +1,97 @@
-let car = {
-    name: 'Lightning McQueen',
-    model: 'Race Car',
-    year: 2006,
-    start: function() {
-        console.log(`Kachow!" ${this.year} ${this.name} is ready to roll!`);
-    },
-
-drive: function() {
-    console.log(`${this.name} is zooming at top speed! Vroom-vroom!`);
-},
-brake: function() {
-    console.log(`${this.name} creeches to a halt!`);
-},
-honk: function() {
-    console.log(`${this.name} honks: "beep beep in the clouds!!`);
-}
-};
-
-
-car.start();
-
-
-let student = {
-    name: "alex",
-    age: 21,
-    enrolled: true,
-    courses: ["javascript", "python", "html"],
-    greet: function() {
-        console.log(`Hello, I am ${this.name}`);
+let book = {
+    title: "Gray Wolf",
+    author: "Dug mc guggie",
+    year: 2012,
+    getSummary: function() {
+        return `${this.title} by ${this.author}, published in the ${this.year}`;
     }
 };
 
-student.city = "New York";
-student.greet();
-student.courses.push("css");
-console.log(student.courses);
-console.log(student);
+console.log(book.getSummary());
 
 
-let person = {
-    name: "alice",
-    age: 40,
-    sex: true
-};
-
-for ( let key in person) {
-    console.log(`${key}: ${person[key]}`);
+let carvana = {
+    name: "Carvana",
+    model: "Vrum Vrum",
+    year:201,
+    start: function() {
+        console.log(`${this.name} is starting to pover up !!!`);
+    }
 }
 
+let monkey = {
+    name: "monkey car",
+    model: "sick poop",
+    year: 3454,
+    start: function() {
+        console.log(`${this.name} is in the broooom!!!!!!! `);
+    }
+}
+
+carvana.start();
+monkey.start();
+
+
+function greet() {
+    console.log(this);
+}
+
+greet();
+
+
+function Car(name, model, year) {
+    this.name = name;
+    this.model = model;
+    this.year = year;
+    this.start = function() {
+        console.log(`${this.name} is starting`);
+    }
+}
+
+let lightning = new Car('Lightnning', 'McQueen', 2006);
+let mater = new Car('Mater', 'Tow Truck', 2002);
+
+lightning.start();
+mater.start();
+
+
+
+let student = {
+    name: "Alice",
+    age: 33,
+    address: {
+        street: "IDK 456 Street",
+        city: "Brooksville",
+        zipCode: '62702'
+    },
+    courses: ['Javacsript', 'HTML', 'PYTHON']
+};
+
+console.log(student.address.city);
+console.log(student.courses[2]);
+
+student.address.city = "New York";
+console.log(student.address.city);
+
+console.log(student);
+
+for (let key in student.address) {
+    console.log(`${key}: ${student.address[key]}`);
+}
+
+let costumer = {
+    name: 'Jane Doe',
+    contact: {
+        phone: '123-456-7890',
+        email: 'jong@gmail.com'
+    },
+    shippingAddress: {
+        street: '123 Main St',
+        city: 'Springfield',
+        state: 'IL',
+        zip: '62701'
+    }
+};
+
+console.log(costumer.contact.email);
+console.log(costumer.shippingAddress.city);
